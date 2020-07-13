4ad898371e85ad87104624b69160e29d5c8f5db5
xql <18815593278@163.com>
2020-07-13 22:20:06 +0800

    利用Java调用python脚本文件demo得到执行返回

--- src/main/java/com/monitor/TaskController.java
+++ src/main/java/com/monitor/TaskController.java
@@ -2,6 +2,7 @@ package com.monitor;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.List;
 
@@ -15,15 +16,23 @@ public class TaskController {
 
     public void run() throws IOException, InterruptedException {
         boolean isWindows = System.getProperty("os.name").toLowerCase().startsWith("windows");
-        System.out.println(isWindows);
-        String homeDirectory = System.getProperty("user.dir");
+        //System.out.println(isWindows);
+        String homeDirectory = String.format("%s\\src\\main\\java\\com\\monitor\\", System.getProperty("user.dir"));
         System.out.println(homeDirectory);
         Process process;
         Runtime rt = Runtime.getRuntime();
         if (isWindows) {
-            process = rt.exec(String.format("java %s","LoopTask.java"));
+            String[] cmd = {"cmd","/C", String.format("python %shello.py", homeDirectory)};
+            process = rt.exec(cmd);
+            InputStream fs = process.getInputStream();
+            InputStreamReader ret = new InputStreamReader(fs);
+            BufferedReader str = new BufferedReader(ret);
+            String line = null;
+            while ((line=str.readLine())!=null){
+                System.out.println(line);
+            }
         } else {
-            process = rt.exec("javac");
+            process = rt.exec("dir");
         }
         BufferedReader input = new BufferedReader(new InputStreamReader(process.getInputStream(), "GBK"));
         int exitCode = process.waitFor();
